6b6eb879c0c4db359ebfd19a2695af59cdac65f7
Oyannax <oyannalopez14@gmail.com>
2023-11-21 23:08:41 +0100

    functional carousel & slider

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+const carousel = document.querySelector('.carousel');
+const firstSlide = carousel.querySelectorAll('.slide')[0];
+const nextSlide = carousel.querySelectorAll('.slide')[1];
+const arrowIcons = document.querySelectorAll('.carousel-controls .arrow');
+const dots = document.querySelectorAll('.carousel-dots .dot');
+
+let target = 0;
+carousel.scrollLeft = 0;
+carousel.addEventListener("scroll", showHideIcons);
+document.addEventListener("scroll", showActiveDots);
+
+let firstSlideWidth = firstSlide.clientWidth + 17;
+let nextSlideWidth = nextSlide.clientWidth + 17;
+let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+
+function showHideIcons() {
+    arrowIcons[0].style.display = carousel.scrollLeft === 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft === scrollWidth ? "none" : "block";
+}
+
+function showActiveDots() {
+    for (let i = 0; i < dots.length; i++) {
+        if (i === target) {
+            dots[i].classList.add("active");
+        } else {
+            dots[i].classList.remove("active");
+        }
+    }
+}
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        if (icon.id === "right") {
+            if (carousel.scrollLeft === 0) {
+                carousel.scrollLeft += firstSlideWidth;
+                target++;
+            } else {
+                carousel.scrollLeft += nextSlideWidth;
+                target++;
+            }
+        } else {
+            carousel.scrollLeft -= nextSlideWidth;
+            target--;
+        }
+        setTimeout(() => showHideIcons(), 60);
+        showActiveDots();
+    })
+})
+
 const toRead = document.querySelector('.to-read-content');
 
 let isDragStart = false;
