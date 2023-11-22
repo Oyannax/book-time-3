ae5e001a1837fac8162fcf8e6dafbdcd87e3b4e6
Oyannax <oyannalopez14@gmail.com>
2023-11-22 09:29:31 +0100

    prevent double click

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const dots = document.querySelectorAll('.carousel-dots .dot');
 
 let target = 0;
 carousel.scrollLeft = 0;
+dots[0].classList.add("active");
+
 carousel.addEventListener("scroll", showHideIcons);
 document.addEventListener("scroll", showActiveDots);
 
@@ -29,21 +31,25 @@ function showActiveDots() {
 }
 
 arrowIcons.forEach(icon => {
-    icon.addEventListener("click", () => {
-        if (icon.id === "right") {
-            if (carousel.scrollLeft === 0) {
-                carousel.scrollLeft += firstSlideWidth;
-                target++;
+    icon.addEventListener("click", (e) => {
+        if (!e.detail || e.detail === 1) {
+            if (icon.id === "right") {
+                if (carousel.scrollLeft === 0) {
+                    carousel.scrollLeft += firstSlideWidth;
+                    target++;
+                } else {
+                    carousel.scrollLeft += nextSlideWidth;
+                    target++;
+                }
             } else {
-                carousel.scrollLeft += nextSlideWidth;
-                target++;
+                carousel.scrollLeft -= nextSlideWidth;
+                target--;
             }
+            setTimeout(() => showHideIcons(), 60);
+            showActiveDots();
         } else {
-            carousel.scrollLeft -= nextSlideWidth;
-            target--;
+            return;
         }
-        setTimeout(() => showHideIcons(), 60);
-        showActiveDots();
     })
 })
 
@@ -74,44 +80,3 @@ function dragging(e) {
 toRead.addEventListener("mousedown", dragStart);
 toRead.addEventListener("mouseup", dragStop);
 toRead.addEventListener("mousemove", dragging);
-
-
-// const carousel = document.getElementById('carousel');
-// const slides = document.getElementById('carouselContent');
-
-// let isPressed = false;
-
-// let cursorX;
-
-// carousel.addEventListener("mousedown", (event) => {
-//     isPressed = true;
-//     cursorX = event.offsetX - slides.offsetLeft;
-//     carousel.style.cursor = "grabbing";
-// })
-
-// carousel.addEventListener("mouseup", () => {
-//     carousel.style.cursor = "grab";
-// })
-
-// carousel.addEventListener("mousemove", (event) => {
-//     if (!isPressed) return;
-//     event.preventDefault();
-//     slides.style.left = `${event.offsetX - cursorX}px`;
-//     boundSlides();
-// })
-
-// window.addEventListener("mouseup", () => {
-//     isPressed = false;
-// })
-
-// function boundSlides() {
-//     const carouselRect = carousel.getBoundingClientRect();
-//     const slidesRect = slides.getBoundingClientRect();
-//     console.log(carouselRect, slidesRect);
-
-//     if (parseInt(slides.style.left) > 0) {
-//         slides.style.left = 0;
-//     } else if (slidesRect.right < carouselRect.right) {
-//         slides.style.left = `-${slidesRect.width - carouselRect.width}px`;
-//     }
-// }
